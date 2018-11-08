a24b39c03c16c6f8dee6fe26e42d3d1a1990e8c0
macross <macrossluosa@icloud.com>
2018-11-08 11:08:54 +0800

    dingding.py可以

---
+++ dingding.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+import sys
+import os
+
+headers = {'Content-Type': 'application/json;charset=utf-8'}
+api_url = "https://oapi.dingtalk.com/robot/send?access_token=7f25442478beca503735e4f8e4bad95e615b3cdc672a1d834e53d9ba3c75f73e"
+
+def msg(text):
+    json_text= {
+     "msgtype": "text",
+        "text": {
+            "content": text
+        },
+        "at": {
+            "atMobiles": [
+                "1881***1223"
+            ],
+            "isAtAll": False
+        }
+    }
+    ## 这是python2的语法；python3的要修改
+    #print requests.post(api_url,json.dumps(json_text),headers=headers).content
+    print(requests.post(api_url,json.dumps(json_text),headers=headers).content)
+
+if __name__ == '__main__':
+    text = sys.argv[1]
+    msg(text)
